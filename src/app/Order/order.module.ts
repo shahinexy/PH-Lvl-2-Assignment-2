69174581c7de88ef64c1bd4c861e6a397db767ca
shahinexy <shahinexy@gmail.com>
2024-12-23 12:59:27 +0600

    order interface and module created

---
+++ src/app/Order/order.module.ts
@@ -0,0 +1,10 @@
+import { Schema } from "mongoose";
+import { Order } from "./order.interface";
+
+
+const OrderSchema = new Schema<Order>({
+    email: {type: String, required: true},
+    product: {type: String, required: true},
+    quantity: {type: Number, required: true},
+    totalPrice: {type: Number, required: true},
+})
